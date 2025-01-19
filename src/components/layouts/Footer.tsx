d7368f044cb650bcf16ea868a173442f87f4d110
Nitipat Punoi <punoi.n@gmail.com>
2025-01-19 15:47:24 +0700

    add layouts component

---
+++ src/components/layouts/Footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <footer className="flex justify-center items-center">
+      <span className="px-1 hidden sm:inline">Copyright</span>
+      <span className="px-1"> &copy;</span>
+      <span className="px-1 hidden sm:inline"> 2025,</span>
+      <span className="px-1"> All Rights Reserved</span>
+    </footer>
+  )
+}
