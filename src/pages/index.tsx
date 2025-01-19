247d87c74bfce746a53f1462d4bad0e17e036a73
Nitipat Punoi <punoi.n@gmail.com>
2025-01-19 15:57:01 +0700

    fix pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import HomeScreen from './HomeScreen/HomeScreen'
+import PlayScreen from './PlayScreen/PlayScreen'
+import SettingScreen from './SettingScreen/SettingScreen'
+import CreditScreen from './CreditScreen/CreditScreen'
+
+export { HomeScreen, PlayScreen, SettingScreen, CreditScreen }
