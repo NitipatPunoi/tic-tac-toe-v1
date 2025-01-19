8511b764389c0b0a47a18eee37afbd044cd1a217
Nitipat Punoi <punoi.n@gmail.com>
2025-01-19 15:56:10 +0700

    add board component

---
+++ src/components/Board.tsx
@@ -0,0 +1,32 @@
+import { Move } from '../types'
+
+type BoardProps = {
+  board: string[][]
+  lastMove: Move | null
+  winningPath: Move[]
+  onClick: (rowIndex: number, colIndex: number) => void
+}
+
+export const Board: React.FC<BoardProps> = ({ board, lastMove, winningPath, onClick }) => {
+  const isWinningPath = (row: number, col: number) => winningPath.some((pos) => pos.row === row && pos.col === col)
+  return (
+    <div className="board select-none">
+      {board.map((row, rowIndex) => (
+        <div key={rowIndex} className="flex flex-row items-center justify-center">
+          {row.map((col, colIndex) => (
+            <div
+              key={colIndex}
+              onClick={() => onClick(rowIndex, colIndex)}
+              className={`flex items-center justify-center w-[50px] h-[50px] text-2xl font-lilita border bg-boardBackground border-boardBorder 
+                ${col !== null ? (col == 'X' ? 'x-mark' : 'o-mark') : 'cursor-pointer'} 
+                ${lastMove && lastMove.row === rowIndex && lastMove.col === colIndex && 'lastMove'}
+                ${isWinningPath(rowIndex, colIndex) && 'hilight'}`}
+            >
+              {col}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+}
