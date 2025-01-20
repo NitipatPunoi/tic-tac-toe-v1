9f058d17bf05161115d88139c55509989a1c8a27
Nitipat Punoi <punoi.n@gmail.com>
2025-01-20 15:01:02 +0700

    add game mode type

---
+++ src/types/gameMode.ts
@@ -0,0 +1,7 @@
+export enum GameModeType {
+  SinglePlayer = 'single-player',
+  MultiPlayer = 'multi-player',
+  Default = 'default',
+}
+
+export type GameMode = GameModeType
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 import { BoardSize, Setting } from './setting'
+import { GameMode, GameModeType } from './gameMode'
 import { Move, BoardType, PlayState, GameState, GameAction, Action, ActionType } from './game'
 
-export { ActionType }
-export type { Move, BoardType, PlayState, GameState, GameAction, Action }
-export type { Setting, BoardSize }
+export { ActionType, GameModeType }
+export type { Setting, BoardSize, GameMode, Move, BoardType, PlayState, GameState, GameAction, Action }
