9c80e63a0067282ee421dcb7209fffc8a1a4786d
Nitipat Punoi <punoi.n@gmail.com>
2025-01-20 15:01:44 +0700

    fix typo

--- src/contexts/SettingContext.tsx
+++ src/contexts/SettingContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, useEffect, ReactNode } from 'react'
 import { SETTING_STORAGE_KEY, DEFAULT_SETTING } from './../config'
-import { Setting } from './../types/setting'
+import { Setting } from './../types'
 import { getFromLocalStorage, saveToLocalStorage } from '../utils'
 
 type SettingContextType = {
