14e742a7005ffd8d41760cb19622f5534ef4c051
Nitipat Punoi <punoi.n@gmail.com>
2025-01-22 05:17:11 +0700

    fix board component use getStepsBack for lastMove

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,18 +1,23 @@
-import { Move, BoardType } from './../types'
+import { getStepsBack } from '../utils'
+import { GameState } from '../types'
 
 type BoardProps = {
-  board: BoardType
-  move: Move | null
-  winningPath: Move[] | null
+  state: GameState
   onClick: (row: number, col: number) => void
 }
 
-export const Board: React.FC<BoardProps> = ({ board, move, winningPath, onClick }) => {
+export const Board: React.FC<BoardProps> = ({ state, onClick }) => {
   const isWinningPath = (row: number, col: number) =>
-    winningPath !== null && winningPath.some((pos) => pos.row === row && pos.col === col)
+    state.play.winningPath !== null && state.play.winningPath.some((pos) => pos.row === row && pos.col === col)
+
+  const isLastMove = (row: number, col: number): string => {
+    const lastMove = getStepsBack(state.logs)
+    return lastMove && lastMove.row === row && lastMove.col === col ? 'lastMove' : ''
+  }
+
   return (
     <div className="board select-none">
-      {board.map((boardRow, row) => (
+      {state.board.map((boardRow, row) => (
         <div key={row} className="flex flex-row items-center justify-center">
           {boardRow.map((cell, col) => (
             <div
@@ -20,7 +25,7 @@ export const Board: React.FC<BoardProps> = ({ board, move, winningPath, onClick 
               onClick={() => onClick(row, col)}
               className={`flex items-center justify-center w-[50px] h-[50px] text-2xl font-lilita border bg-boardBackground border-boardBorder 
                 ${cell !== null ? (cell == 'X' ? 'x-mark' : 'o-mark') : 'cursor-pointer'} 
-                ${move && move.row === row && move.col === col && 'lastMove'}
+                ${isLastMove(row, col)}  
                 ${isWinningPath(row, col) && 'hilight'}`}
             >
               {cell}
